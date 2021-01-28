1898203e3b2c282a075824203305a68200f1fa1b
Contraxton <78047425+Contraxton@users.noreply.github.com>
2021-01-27 18:16:16 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -4,22 +4,60 @@ namespace myImages {
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "image1":return img`
-. . . . . f f f f f . . . . . . 
-. . . . f d b b b b f . . . . . 
-. . . . f b b b b b f . . . . . 
-. . . . f b f f f f f . . . . . 
-. . . . f b b b f b f . . . . . 
-. . . . f b b b f b f . . . . . 
-. . . . f b b b f b f . . . . . 
-. . . f d f f f f f d f . . . . 
-. . f d d b e e e b d d f . . . 
-. . f e b b b e b b b e f . . . 
-. . f e f e b b b e f e f . . . 
-. . f f f e b b b e f f f . . . 
-. . b b f c c d c c f b b . . . 
-. . . . f e b b b e f . . . . . 
-. . . . f f f f f f f . . . . . 
-. . . . . e e . e e . . . . . . 
+.....fffff..........
+....fd6666f.........
+....f66666f.........
+....f6fffff.........
+....f666f6f.........
+....f666f6f.........
+....f666f6f.fff.....
+...fdfffffdffd7fff2.
+..fdd6eee6dedddddd2.
+..fe666e666ebdddddf.
+..fefe666effbdfffff.
+..fffe666ef.fdf.....
+..bbfccdccf.fff.....
+....fe666ef.........
+....fffffff.........
+.....ee.ee..........
+....................
+....................
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. 2 2 2 2 5 5 1 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":return img`
+. . . . . . f f f f f . . . . . 
+. . . . . f f . . . f f . . . . 
+. . f f f f . . . . . f f . . . 
+. f f . . f f . . . f . . f . . 
+. f . . . . f f . f f . . f f . 
+f f . . . f . . f f . f . . f . 
+f . . . . . . . . . . f . . f . 
+f . . . . . . . . . . . . . f . 
+. f . . . f f f f f f . . . f . 
+. f . . . f . . . . f . . . f . 
+. f f . . f . . . . f . . f . . 
+. . f f . . f . . . . . f f . . 
+. . . f f f . . . . f f f . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,20 +1,83 @@
-let mySprite = sprites.create(img`
-    . . . . . f f f f f . . . . . . 
-    . . . . f d b b b b f . . . . . 
-    . . . . f b b b b b f . . . . . 
-    . . . . f b f f f f f . . . . . 
-    . . . . f b b b f b f . . . . . 
-    . . . . f b b b f b f . . . . . 
-    . . . . f b b b f b f . . . . . 
-    . . . f d f f f f f d f . . . . 
-    . . f d d b e e e b d d f . . . 
-    . . f e b b b e b b b e f . . . 
-    . . f e f e b b b e f e f . . . 
-    . . f f f e b b b e f f f . . . 
-    . . b b f c c d c c f b b . . . 
-    . . . . f e b b b e f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . . e e . e e . . . . . . 
+namespace SpriteKind {
+    export const badGuy = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.badGuy, function (sprite, otherSprite) {
+    game.over(false)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.pewPew.play()
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . 2 2 2 2 5 5 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 50, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.badGuy, function (sprite, otherSprite) {
+    tiles.setTilemap(tilemap`level5`)
+    mySprite2.setPosition(34896053, 8345)
+    mySprite.setPosition(34896053, 6.386084765845897e+23)
+    projectile.setPosition(34896053, 8345)
+    game.showLongText("loading next round...", DialogLayout.Bottom)
+    tiles.setTilemap(tilemap`level2`)
+    mySprite2.setPosition(91, 4)
+    mySprite.setPosition(76, 34)
+})
+let projectile: Sprite = null
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    .....fffff..........
+    ....fd6666f.........
+    ....f66666f.........
+    ....f6fffff.........
+    ....f666f6f.........
+    ....f666f6f.........
+    ....f666f6f.fff.....
+    ...fdfffffdffd7fff2.
+    ..fdd6eee6dedddddd2.
+    ..fe666e666ebdddddf.
+    ..fefe666effbdfffff.
+    ..fffe666ef.fdf.....
+    ..bbfccdccf.fff.....
+    ....fe666ef.........
+    ....fffffff.........
+    .....ee.ee..........
+    ....................
+    ....................
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
+mySprite2 = sprites.create(img`
+    . . . . . . f f f f f . . . . . 
+    . . . . . f f . . . f f . . . . 
+    . . f f f f . . . . . f f . . . 
+    . f f . . f f . . . f . . f . . 
+    . f . . . . f f . f f . . f f . 
+    f f . . . f . . f f . f . . f . 
+    f . . . . . . . . . . f . . f . 
+    f . . . . . . . . . . . . . f . 
+    . f . . . f f f f f f . . . f . 
+    . f . . . f . . . . f . . . f . 
+    . f f . . f . . . . f . . f . . 
+    . . f f . . f . . . . . f f . . 
+    . . . f f f . . . . f f f . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.badGuy)
+mySprite2.follow(mySprite, 50)
+mySprite2.setPosition(91, 4)
